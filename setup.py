fc60700643a9fe75b7ebd18d3768c1dbbcd0b22a
fastily <fastily@users.noreply.github.com>
2024-04-22 01:46:38 -0700

    tag for release - 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mtc-cli",
-    version="0.1.0",
+    version="0.2.0",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="A simple CLI tool for moving enwp files to Commons",
